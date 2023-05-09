cd0b3d175d17b7d585d57764341e792c99d79be9
303 - Programacion <ndcastillor@gmail.com>
2023-05-09 14:53:56 -0500

    Clases_18

---
+++ Clases/Sesion_19.py
@@ -0,0 +1,36 @@
+import sys
+import PyQt5.QtWidgets as PyQT
+from PyQt5  import uic
+
+class principal(PyQT.QMainWindow):
+    def __init__(self):
+        super().__init__()
+        self.initGuit()
+
+    def initGuit(self):
+        uic.loadUi('PrimerEjercicio.ui',self)
+        self.show()
+
+        self.pushButton.clicked.connect(lambda:self.calcular())#lambda no se para que es, lambda y () se puede quitar
+
+    def calcular(self):
+        texto1=float(self.num1.text())
+        texto2=float(self.num2.text())
+
+        if self.sumar.isChecked()==True:
+            self.resultado.setText(str(texto1+texto2))
+        elif self.restar.isChecked()==True:
+            self.resultado.setText(str(texto1-texto2))
+        elif self.multiplicar.isChecked()==True:
+            self.resultado.setText(str(texto1*texto2))
+        else:
+            self.resultado.setText(str(texto1/texto2))
+
+
+def main():
+    app = PyQT.QApplication([])
+    window =  principal()
+    sys.exit(app.exec_())
+
+if __name__=="__main__":
+    main()
